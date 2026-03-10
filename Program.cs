70cfc73169eced664f7fc54e8cf5e5784df64b81
Oleksandr Oliinyk <s32750@pjwstk.edu.pl>
2026-03-10 13:44:21 +0100

    fake commit 2

--- Program.cs
+++ Program.cs
@@ -4,3 +4,5 @@ List<int> valuesList = new List<int> {1,2,3,4,5,6,7};
 valuesList.ForEach(x => Console.WriteLine(x));
 
 Console.WriteLine("My index number is 32750");
+
+Console.WriteLine("fake commmit 1");
