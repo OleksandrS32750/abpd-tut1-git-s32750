72d0d0dcd0087af50054b983740850023c3b0278
Oleksandr Oliinyk <s32750@pjwstk.edu.pl>
2026-03-10 13:35:52 +0100

    valuesList commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+List<int> valuesList = new List<int> {1,2,3,4,5,6,7};
+
+valuesList.ForEach(x => Console.WriteLine(x));
