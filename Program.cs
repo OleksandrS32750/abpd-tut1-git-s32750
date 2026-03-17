2aa20e3a200443cfad68c9f2bbbe55f419b5d27b
OleksandrS32750 <s32750@pjwstk.edu.pl>
2026-03-17 10:56:20 +0100

    added CalculateMin function

--- Program.cs
+++ Program.cs
@@ -30,3 +30,13 @@ int CalculateMax(int[] values)
 
     return max;
 }
+
+int CalculateMin(int[] values)
+{
+    int min = values[0];
+    for (int i = 1; i < values.Length; i++)
+        if (values[i] < min)
+            min = values[i];
+
+    return min;
+}
